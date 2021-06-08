41222ca087b4013252b13d21e971fe43f6d7cd92
sethorpe <oluseyi.thorpe@gmail.com>
2021-06-08 11:24:53 -0700

    Add Edit Favs Title method

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 import shutil
 import utility
 import unittest
+import pytest
 from time import sleep
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -23,7 +24,7 @@ class TestTranslinkAuto(unittest.TestCase):
 
     def setUp(self):
         options = Options()
-        options.add_argument("--headless")
+        # options.add_argument("--headless")
         options.add_argument("--no-sandbox")
         self.driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
         self.driver.implicitly_wait(10)
@@ -44,7 +45,7 @@ class TestTranslinkAuto(unittest.TestCase):
         for method, error in self._outcome.errors:
             if error:
                 self.driver.get_screenshot_as_file(os.getcwd() + "/Translink_Errors/" + "screenshot_" + self.id() + "__" + utility.current_time() + ".png")
-
+    
     def test_find_bus_route(self):
         self.driver.find_element(By.LINK_TEXT,'Next Bus').click()
 
@@ -56,7 +57,7 @@ class TestTranslinkAuto(unittest.TestCase):
         self.driver.find_element(By.CSS_SELECTOR, ".verticallyCenteredContent > button").click()
         self.driver.get_screenshot_as_file(os.getcwd() + "/Translink_Screenshots/" + "screenshot_" + self.id() + "__" + utility.current_time() + ".png")
 
-
+   
     def test_find_bus_route_and_save_to_favs(self):
         self.driver.find_element(By.LINK_TEXT,'Next Bus').click()
 
@@ -81,6 +82,51 @@ class TestTranslinkAuto(unittest.TestCase):
         self.driver.find_element(By.XPATH, '//button[contains(text(),\'Add to Favourites\')]').click()
         # Wait 5 seconds before moving on
         sleep(5)
+
+    def test_validate_edit_favs(self):
+        self.driver.find_element(By.LINK_TEXT,'Next Bus').click()
+
+        # Find the search box, first clear it and then enter search parameter '99'
+        self.driver.find_element_by_id('NextBusSearchTerm').clear()
+        self.driver.find_element_by_id('NextBusSearchTerm').send_keys('99')
+
+        # Click on 'Find My Next Bus'
+        self.driver.find_element(By.CSS_SELECTOR, ".verticallyCenteredContent > button").click()
+
+        # Click on "Add Fav" Icon
+        self.driver.find_element(By.CLASS_NAME, 'AddDelFav').click()
+
+        # Click the dialog box
+        self.driver.find_element(By.ID, "add-to-favourites_dialog").click()
+
+        # First clear the text field, then type in "TransLink Auto Homework"
+        self.driver.find_element(By.NAME, 'newFavourite').clear()
+        self.driver.find_element(By.NAME, 'newFavourite').send_keys('Translink Auto Homework')
+
+        # Click on Add to Favorites button to save
+        self.driver.find_element(By.XPATH, '//button[contains(text(),\'Add to Favourites\')]').click()
+        # Wait 5 seconds before moving on
+        sleep(5)
+
+        # Click on "My Favs" icon
+        self.driver.find_element(By.LINK_TEXT, 'My Favs').click()
+
+        # Click on 'Edit' button
+        self.driver.find_element(By.CLASS_NAME, 'editFavouriteButton').click()
+
+        # Change the favourite name
+        self.driver.find_element(By.NAME, "currentFavourite").clear()
+        self.driver.find_element(By.NAME, 'currentFavourite').send_keys('99 Commercial to UBC')
+
+        # Save the new name
+        self.driver.find_element(By.NAME, 'btnEditFav').click()
+
+        # Validate the name change
+        my_route = self.driver.find_element(By.LINK_TEXT, '99 Commercial to UBC')
+        self.assertEqual(my_route.text, '99 Commercial to UBC', 'Link text does not match expected result')
+        self.driver.get_screenshot_as_file(os.getcwd() + "/Translink_Screenshots/" + "screenshot_" + self.id() + "__" + utility.current_time() + ".png")
+
+
 
     def test_validate_favs_link_title(self):
         self.driver.find_element(By.LINK_TEXT,'Next Bus').click()
@@ -126,6 +172,7 @@ class TestTranslinkAuto(unittest.TestCase):
         # Now get the current url and compare with param my_route_url from earlier.
         self.assertEqual(self.driver.current_url, my_route_url, 'URLs do not match!')
 
+    
     def test_validate_route_title_displayed(self):
         self.driver.find_element(By.LINK_TEXT,'Next Bus').click()
 
